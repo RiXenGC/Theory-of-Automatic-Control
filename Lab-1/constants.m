9b63c741dbc78834334a45e12680081be8a015c8
Roman-RiXenGC <r.sadovec0403@yandex.ru>
2024-02-28 14:21:07 +0300

    Added a viscous friction in model of DC Motor

--- Lab-1/constants.m
+++ Lab-1/constants.m
@@ -4,3 +4,6 @@ C_omega = 3.15; % Construction coef of motor, V * s / ( A * Gn )
 L_a = 0.0015; % Anchor inductivity, Gn
 R_a = 8; % Anchor resistance, Om
 L_ew = 0.013; % Inductivity of excitation winding, Gn
+
+%Viscous friction
+Betta = 0.001; %coef of viscoys friction
